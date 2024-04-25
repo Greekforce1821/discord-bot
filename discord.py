7166449633221234fbc363e08e167646d7bb6f29
Spyridon Eftychios Kokotos <spiroskoeu@gmail.com>
2024-04-25 18:00:24 +0300

    Add files via upload

--- discord.py
+++ discord.py
@@ -5,7 +5,7 @@ client = discord.Client()
 @client.event
 async def on_member_join(member):
     for channel in member.guild.channels:
-        if channel.name == 'welcome':  
+        if channel.name == 'waitingroom':  
             await channel.send(f'Καλώς ορίσατε στον server μας, {member.mention}! Παρακαλώ αναμείνατε από τους moderators να σας δώσουν πρόσβαση στο κανάλι iw2024volunteers. Aπολαύστε την τη διαμονή σου σας! Με εκτίμηση, To Helpdesk του Ionian Wikithon')
 
 
