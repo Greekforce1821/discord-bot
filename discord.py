a236eecc9186d01c131df490324b7192a9931a2d
Spyridon Eftychios Kokotos <spiroskoeu@gmail.com>
2024-04-25 23:27:53 +0300

    Update discord.py

--- discord.py
+++ discord.py
@@ -5,7 +5,7 @@ client = discord.Client()
 @client.event
 async def on_member_join(member):
     for channel in member.guild.channels:
-        if channel.name == 'waitingroom':  
+        if channel.name == 'waiting-room':  
             await channel.send(f'Καλώς ορίσατε στον server μας, {member.mention}! Παρακαλώ αναμείνατε από τους moderators να σας δώσουν πρόσβαση στο κανάλι iw2024volunteers. Aπολαύστε την τη διαμονή σου σας! Με εκτίμηση, To Helpdesk του Ionian Wikithon')
 
 
